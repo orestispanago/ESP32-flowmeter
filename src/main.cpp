04fa52884f0d9f2c83d86e3307d19f4bb47d8948
Orestis <orestispanago@gmail.com>
2021-05-25 13:33:30 +0300

    Tidy-up

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,9 @@
 #include <Arduino.h>
-#define FLOWMETER_PIN 13 // As marked on board
+#define FLOWMETER_PIN 17 // As marked on board
 
-long previousMillis;
-unsigned int interval = 5000; // milliseconds
+unsigned long previousMillis, currentMillis, pulseCount;
+unsigned long interval = 1000; // milliseconds
 float calibrationFactor = 4.5;
-unsigned int pulseCount;
-unsigned int pulsesAtInverval;
 float flowRate;
 
 void IRAM_ATTR pulseCounter()
@@ -13,7 +11,7 @@ void IRAM_ATTR pulseCounter()
   pulseCount++;
   if (pulseCount == 0)
   {
-    Serial.println("pulseCount reset to 0 due to overflow. Make sure you are using the correct data type.");
+    Serial.println("pulseCount reset to 0 due to overflow.");
   }
 }
 
@@ -26,20 +24,20 @@ void setup()
 
 void loop()
 {
-  if (millis() - previousMillis > interval)
+  currentMillis = millis();
+  if (currentMillis - previousMillis >= interval)
   {
     Serial.print("pulseCount: \t");
     Serial.println(pulseCount);
-    pulsesAtInverval = pulseCount;
-    pulseCount = 0;
 
     // Because this loop may not complete in exactly 1 second intervals we calculate
     // the number of milliseconds that have passed since the last execution and use
     // that to scale the output. We also apply the calibrationFactor to scale the output
     // based on the number of pulses per second per units of measure (litres/minute in
     // this case) coming from the sensor.
-    flowRate = ((1000.0 / (millis() - previousMillis)) * pulsesAtInverval) / calibrationFactor;
-    previousMillis = millis();
+    flowRate = ((1000.0 / (currentMillis - previousMillis)) * pulseCount) / calibrationFactor;
+    previousMillis = currentMillis;
+    pulseCount = 0;
 
     Serial.print("Flow rate: ");
     Serial.print(flowRate);
